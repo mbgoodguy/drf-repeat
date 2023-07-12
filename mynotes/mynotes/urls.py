a936aa9a0a21ffcfea0e2f663e25b1145b549218
mbgoodguy <nerooren4001@gmail.com>
2023-07-12 18:14:19 +0300

    Add swagger for documentation generation

--- mynotes/mynotes/urls.py
+++ mynotes/mynotes/urls.py
@@ -15,9 +15,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="MyNotesAPI",
+        default_version='v1',
+        description="MyNotesAPI description",
+        terms_of_service="https://www.google.com/policies/terms/",
+        contact=openapi.Contact(email="nerooren4001@gmail.com"),
+        license=openapi.License(name="BSD License"),
+    ),
+    public=True,
+)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('notes.urls', namespace='api')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
